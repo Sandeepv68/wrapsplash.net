ffca21c6f844d9422d4962acb93a91313e6281e1
Sandeep Vattapparambil <7328658+Sandeepv68@users.noreply.github.com>
2026-07-21 12:53:23 +0530

    version fix

--- WrapSplash.NET.Tests/obj/Debug/net8.0/WrapSplash.NET.Tests.AssemblyInfo.cs
+++ WrapSplash.NET.Tests/obj/Debug/net8.0/WrapSplash.NET.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WrapSplash.NET.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91146bb67097a8463aa14140ef3e4eae67cb8ab1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a45d6fd10e7922edb6ce3ccce32253735965c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WrapSplash.NET.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WrapSplash.NET.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WrapSplash.NET/obj/Debug/net8.0/WrapSplash.NET.AssemblyInfo.cs
+++ WrapSplash.NET/obj/Debug/net8.0/WrapSplash.NET.AssemblyInfo.cs
@@ -14,11 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute(("A simple .NET wrapper module for the Unsplash API. Port of the popular wrapsplash" +
     " npm package."))]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("5.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("5.0.0+91146bb67097a8463aa14140ef3e4eae67cb8ab1")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a45d6fd10e7922edb6ce3ccce32253735965c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WrapSplash.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WrapSplash.NET")]
-[assembly: System.Reflection.AssemblyVersionAttribute("5.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/SandeepVattapparambil/wrapsplash.git")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("WrapSplash.NET.Tests")]
 
